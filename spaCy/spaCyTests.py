5b3b31f0b8c29fe0df382fdf7e666af584f128f5
barth010may <borup.rasmus@gmail.com>
2023-10-24 10:50:36 +0200

    Adding testing to spaCy module

---
+++ spaCy/spaCyTests.py
@@ -0,0 +1,13 @@
+import unittest
+import spaCy
+
+class MyTestCase(unittest.TestCase):
+    def test_correct_output(self):
+        self.assertEqual(spaCy.extract_entities("hvor er Obama født?"), ['Obama'])  # add assertion here
+        self.assertEqual(spaCy.extract_entities("Aalborg er i Danmark"), ['Aalborg', 'Danmark'])  # add assertion here
+        self.assertEqual(spaCy.extract_entities("Hvordan åbner man en flaske vin?"), [])  # add assertion here
+        self.assertEqual(spaCy.extract_entities("Jeg bor i New York"), ['New York'])
+        self.assertEqual(spaCy.extract_entities("Hvem er Elon Musk"), ['Elon Musk'])
+
+if __name__ == '__main__':
+    unittest.main()
