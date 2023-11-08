1c107a1b2e6e1b528ea570180f3a0ea2fece59fb
Thomas Ilum Andersen <Thomas.ilum.andersen@gmail.com>
2023-11-08 15:40:18 +0100

    knowledge_retriever file now changed to an API, changed dockerfiles and dockercompose files

--- KnowledgeRetriever/knowledge_retriever.py
+++ KnowledgeRetriever/knowledge_retriever.py
@@ -1,25 +1,30 @@
 # from spaCy.spaCy import extract_entities
+from flask import Flask, request, jsonify
 import requests
 import json
 
+krEndpoint = Flask(__name__)
 
 # Function to call the extract_entities API
 def call_extract_entities(input_string):
     # Define the URL of the extract_entities API
-    url = 'http://spacy:5000/extract_entities'
+    url = 'http://localhost:5000/extract_entities'
     # Define the headers for the request
     headers = {'Content-Type': 'application/json'}
     # Define the data for the request
     data = {'input_string': input_string}
     # Send a POST request to the extract_entities API
+    print(json.dumps(data))
     response = requests.post(url, headers=headers, data=json.dumps(data))
+
     # If the request was successful, return the response as a Python object
     if response.status_code == 200:
         return response.json()
     # If the request failed, print an error message and return None
     else:
         print(f'Request failed with status code {response.status_code}')
-        return none
+        return None
+
 
 # Nedestående funktioner kan bare slettes og erstattes med de rigtige :)
 
@@ -55,11 +60,37 @@ def getChatbotResponse(processedData):
     raise Exception("'getChatbotResponse' Not yet implemented")
     return response
 
-# Get input from the user
-userInput = promptUser()
 
-# Call the extract_entities API in the spacy service to get keywords from the user input
-keywords = call_extract_entities(userInput)
+"""flag = True
+while flag:"""
+# Get input from the user
+#userInput = promptUser()
+
+# Define a route for the API. This will be the endpoint for the function.
+@krEndpoint.route('/knowledge_retriever', methods=['POST'])
+def knowledge_retriever():
+    # Get the input string from the JSON body of the request.
+    userInput = request.json['input_string']
+    # Call the extract_entities API in the spacy service to get keywords from the user input
+    keywords = call_extract_entities(userInput)
+    #print(keywords)
+
+    return jsonify(keywords)
+
+
+# Run the Flask app.
+if __name__ == '__main__':
+    krEndpoint.run(host='0.0.0.0', port=5000)
+
+
+
+
+
+"""repeat = "flag"
+while repeat != "y" and repeat != "n":
+    repeat = input("Har du lyst til at spørge om noget nyt? (y/n)")
+if repeat == "n":
+    flag = False"""
 
 # Placeholder code for further processing and response generation
 # processedKeywords = processKeywords(keywords)
--- spaCy/spaCy.py
+++ spaCy/spaCy.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 import spacy
 
-app = Flask(__name__)
+spacyEndpoint = Flask(__name__)
 nlp = spacy.load('da_core_news_md')
 
 
 # Define a route for the API. This will be the endpoint for the function.
-@app.route('/extract_entities)', methods=['POST'])
+@spacyEndpoint.route('/extract_entities', methods=['POST'])
 def extract_entities():
     # Get the input string from the JSON body of the request.
     input_string = request.json['input_string']
@@ -20,4 +20,4 @@ def extract_entities():
 
 # Run the Flask app.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    spacyEndpoint.run(host='0.0.0.0', port=5000)
