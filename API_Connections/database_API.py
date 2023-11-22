6d55c5293fcf5b645265a976d081eecebd45cb31
Ellleby <charlotteelleby@gmail.com>
2023-11-22 12:04:46 +0100

    Database endpoint fixed.
    Added two get request pr. entity.

--- API_Connections/database_API.py
+++ API_Connections/database_API.py
@@ -1,5 +1,38 @@
-import request
+import requests
+keywords = ["Aalborg", "Berlin"]
+url = "http://localhost:8000/knowledge-base"
 
-response = requests.get('http://localhost:8000/get', verify=False) #only works if you are connected to server: ssh <studiemail>@knox-kb01.srv.aau.dk -L 8000:localhost:8081.
-print(response.status_code)
-print(response.json())
+
+def add_url_query(keyword, type):
+    if (not(type == "subject" or type == "object")):
+        raise Exception("type has to be subject or object.")
+
+    if type == "subject":
+        print(url + "?s=" + keyword)
+        return url + "?s=" + keyword
+
+    elif type == "object":
+        print(url + "?o=" + keyword)
+        return url + "?o=" + keyword
+
+def call_database_API():
+    for entity in keywords:
+        # Add each entity add as subject and object
+        subURL = add_url_query(entity, "subject")
+        objURL = add_url_query(entity, "object")
+
+        response = requests.get(subURL,
+                                verify=False)  # only works if you are connected to KNOXserver: ssh <studiemail>@knox-kb01.srv.aau.dk -L 8000:localhost:8081.
+        print(response.status_code)
+        print(response.json())
+
+        response2 = requests.get(objURL,
+                                verify=False)  # only works if you are connected to KNOXserver: ssh <studiemail>@knox-kb01.srv.aau.dk -L 8000:localhost:8081.
+        print(response2.status_code)
+        print(response2.json())
+
+call_database_API()
+
+
+
+#s=aablorg?s=berlin?o=aalborg?o=berlin
